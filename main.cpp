1a47dee1a247b7d59b62448aa9f2a5bd068d1369
Andrey22898742352 <125690125+Andrey22898742352@users.noreply.github.com>
2024-04-29 12:34:38 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+// Класс 1
+class shepherd {
+public:
+    
+   std::string smart;
+    
+};
+
+
+// Класс 2
+class chihuahua{
+public:
+
+    std::string small; 
+};
+
+
+// Класс 3
+class doberman{
+public:
+
+    std::string fast;
+};
+
+
+// Класс 4
+class Labrador{
+public:
+
+    std::string Beautiful;
+};
+
+// Класс 4
+class bloodhounds{
+
+
+}
